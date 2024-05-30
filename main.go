004db5e5e757be756983fe537167a7f2aeaec811
Pedro Pires <ph_pires@poli.ufrj.br>
2024-05-30 11:09:37 -0300

    creating task list
    function that creates a task list file to get the feeling

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	Name   string
+	Order  int
+	IsDone bool
+}
+
+var taskListFileName string = "task-list"
+var newTask string
+var todoList Task
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func createFileLine(task Task) string {
+	return fmt.Sprintf("%d %s %t", task.Order, task.Name, task.IsDone)
+}
+
+func write(f *os.File, task Task) {
+	line := createFileLine(task)
+	w := bufio.NewWriter(f)
+	bw, err := w.WriteString(line)
+	check(err)
+	fmt.Printf("wrote %d bytes.", bw)
+	w.Flush()
+
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	flag.StringVar(&newTask, "t", "Clean your room", "Add a task to complete.")
+	flag.Parse()
+
+	todoList.Name = newTask
+	todoList.Order = 1
+	todoList.IsDone = false
+
+	fmt.Println("New task added: " + newTask)
+
+	f, err := os.Create(taskListFileName)
+	check(err)
+
+	defer f.Close()
+	write(f, todoList)
 }
